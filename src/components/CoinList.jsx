18c29566dde84aa9a5a7020a067eca0736c4d748
RobsondCCarneiro <robinho.cenep@gmail.com>
2022-06-17 00:54:53 -0300

    Utilizando API para exibir as critpto-moedas

---
+++ src/components/CoinList.jsx
@@ -0,0 +1,78 @@
+import React, { useState, useEffect } from 'react';
+import Coin from "./Coin";
+
+const CoinList = () => {
+
+    const [coinList, setCoinList] = useState([])
+    const [coinName, setCoinName] = useState('')
+
+    const mainContainer = {
+        display: 'flex',
+        flexDirection: 'column'
+    }
+
+    const url = "https://api.coinstats.app/public/v1/coins?skip=0"
+
+    useEffect(() => {
+        fetch(url)
+            .then(
+                (result) => result.json()
+            )
+            .then(
+                (data) => setCoinList(data.coins)
+            )
+            .catch(
+                (err) => console.log(`Error: ${err}`)
+            )
+    }, [])
+
+    const filteredCoins = coinList.filter(
+        (coin) => {
+            return coin.name.toLowerCase().includes(coinName.toLowerCase())
+        }
+    )
+
+
+        /* const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        const filteredNumbers = numbers.filter(
+            (num) => {
+                return num > 5
+            }
+        )
+
+        console.log("NUMBERS: ", numbers)
+        console.log("F-NUMBERS: ", filteredNumbers) */
+
+    return (
+        <div style={mainContainer}>
+            <div>
+                <input type="text"
+                    placeholder="Moeda"
+                    onChange={
+                        (event) => {
+                            setCoinName(event.target.value)
+                        }
+                    }
+                />
+            </div>
+            <div>
+                {
+                    filteredCoins.map(
+                        (coin) => {
+                            return (
+                                <Coin 
+                                    name={coin.name}
+                                    icon={coin.icon}
+                                    price={coin.price}
+                                    symbol={coin.symbol}
+                                />
+                            )
+                        }
+                    )
+                }
+            </div>
+        </div>
+    )
+}
+
+export default CoinList
